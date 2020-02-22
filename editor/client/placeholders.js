fb6be404a40044cd7321fe9cf010ec6be9b05ec7
Damiaan Dufaux <damiaan@dpro.be>
2020-02-22 13:55:55 +0100

    Add placeholders hierarchically

--- editor/client/placeholders.js
+++ editor/client/placeholders.js
@@ -11,35 +11,40 @@ function addPlace(editor, ...keyPath) {
     }
 }
 
+function addPlaces(dictionary, path = []) {
+    for (const [field, value] of Object.entries(dictionary)) {
+        if (value instanceof SegmentEditor) {
+            addPlace(value, ...path.concat(field))
+        } else {
+            addPlaces(value, path.concat(field))
+        }
+    }
+}
+
 export function findPlaceHolders() {
     const dq = (query) => document.querySelector(query)
 
-    const firstReadingReferenceEditor = dq('.first-reading.reference').pmEditor
-    addPlace(firstReadingReferenceEditor, 'english', 'readings', 0, 'reference')
-    addPlace(firstReadingReferenceEditor, 'dutch',   'readings', 0, 'reference')
-    addPlace(dq('.first-reading .reading-editor.dutch').pmEditor,   'dutch',   'readings', 0, 'text')
-    addPlace(dq('.first-reading .reading-editor.english').pmEditor, 'english', 'readings', 0, 'text')
-
-    const referenceEditor = dq('.psalm .reference').pmEditor
-    addPlace(referenceEditor, 'english', 'psalm', 'reference')
-    addPlace(referenceEditor, 'dutch',   'psalm', 'reference')
-    addPlace(dq('.psalm .response.english').pmEditor, 'english', 'psalmResponse')
-    addPlace(dq('.psalm .response.dutch').pmEditor,   'dutch',   'psalmResponse')
-    addPlace(dq('.psalm .reading-editor.dutch').pmEditor,   'dutch',   'psalm', 'text')
-    addPlace(dq('.psalm .reading-editor.english').pmEditor, 'english', 'psalm', 'text')
-
-    const verseBeforeGospel = document.querySelector('.verse-before-gospel')
-    const verseBeforeGospelReference = verseBeforeGospel.querySelector('.reference').pmEditor
-    addPlace(verseBeforeGospelReference, 'english', 'verseBeforeGospelReference')
-    addPlace(verseBeforeGospelReference, 'dutch',   'verseBeforeGospelReference')
-    addPlace(verseBeforeGospel.querySelector('.dutch').pmEditor, 'dutch', 'verseBeforeGospel')
-    addPlace(verseBeforeGospel.querySelector('.english').pmEditor, 'english', 'verseBeforeGospel')
-
-    const gospelReference = document.querySelector('.gospel .reference').pmEditor
-    addPlace(gospelReference, 'english', 'gospel', 'reference')
-    addPlace(gospelReference, 'dutch',   'gospel', 'reference')
-    addPlace(document.querySelector('.gospel .dutch.reading-editor').pmEditor,   'dutch',   'gospel', 'text')
-    addPlace(document.querySelector('.gospel .english.reading-editor').pmEditor, 'english', 'gospel', 'text')
+    for (const language of ['english', 'dutch']) {
+        addPlaces({
+            readings: [
+                {
+                    text: dq(`.first-reading .reading-editor.${language}`).pmEditor,
+                    reference: dq('.first-reading.reference').pmEditor
+                }
+            ],
+            psalm: {
+                text: dq(`.psalm .reading-editor.${language}`).pmEditor,
+                reference: dq('.psalm .reference').pmEditor
+            },
+            psalmResponse: dq(`.psalm .response.${language}`).pmEditor,
+            verseBeforeGospel: dq(`.verse-before-gospel .${language}`).pmEditor,
+            verseBeforeGospelReference: dq('.verse-before-gospel .reference').pmEditor,
+            gospel: {
+                text: dq(`.gospel .reading-editor.${language}`).pmEditor,
+                reference: dq('.gospel .reference').pmEditor
+            }
+        }, [language])
+    }
 
     console.log(places)
 }
